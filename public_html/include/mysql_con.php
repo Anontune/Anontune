553c73b30fba90993debbdce5a63dcd938c05487
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-04-24 02:17:20 +0200

    Updated copyright and licensing information

--- public_html/include/mysql_con.php
+++ public_html/include/mysql_con.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ *  This file is part of Anontune.
+ *
+ *  Anontune is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Anontune is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with Anontune.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  (c) 2011 Anontune developers
+ * 
+ */
+ 
 #Connect to DB.
 $mysql_con = mysql_connect($config->database->host, $config->database->username, $config->database->password);
 if($mysql_con == FALSE)
