40a5e616a9eced0909334ab0d77d825398da44bc
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-04-25 13:56:38 +0200

    Forgotten files

---
+++ public_html/api/api.get.playlist.item.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ *  This file is part of Anontune.
+ *
+ *  Anontune is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Anontune is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with Anontune.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
+
+if($_ANONTUNE !== true) { die(); }
+
+$sPlaylistItem = new PlaylistItem($router->uParameters[1]);
+
+$sData[0] = $sPlaylistItem->Export();
+$sStatus = ANONTUNE_API_SUCCESS;
+?>
